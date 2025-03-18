b557bcd677fa3dcf35067a32723f68993925f2f5
sunkesruthi <sunkesruthi@gmail.com>
2025-03-18 18:52:42 +0530

    Order Online

--- script.js
+++ script.js
@@ -123,3 +123,11 @@ anchorLinks.forEach(link => {
         });
     });
 });
+
+function openModal() {
+  document.getElementById("modal1").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal1").style.display = "none";
+}
